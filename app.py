b0561cf9b0aab070cdc0708d3d4380497ebcd70b
cnarendhran <35360022+cnarendhran@users.noreply.github.com>
2023-12-14 00:00:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ import streamlit as st
 def largest(a, b, c):
   return max(a, b, c)
 
+st.header('**App to find the largest among three**', divider='rainbow')
+
 # Create input fields for user to enter values
-num1 = st.number_input("Enter first number:", key="num1")
-num2 = st.number_input("Enter second number:", key="num2")
-num3 = st.number_input("Enter third number:", key="num3")
+num1 = st.number_input("**Enter first number:**", key="num1")
+num2 = st.number_input("**Enter second number:**", key="num2")
+num3 = st.number_input("**Enter third number:**", key="num3")
 
 # Calculate the largest
 largest_num = largest(num1, num2, num3)
 
 # Display the result
-st.write(f":rainbow[The largest number is:] {largest_num}")
+st.write(":blue[**The largest number is:**]",largest_num)
